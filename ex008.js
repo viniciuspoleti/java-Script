b629093b4f5b652c9b0b34d35251f63fd3e4687d
Vinicius Poleti <poleti.vinicius@gmail.com>
2022-02-21 18:01:08 -0300

    ex9010

---
+++ ex008.js
@@ -0,0 +1,6 @@
+var vel = 500
+console.log(`a velocidade do seu carro é ${vel}Km/H`)
+if (vel > 60){
+    console.log('Você ultrapassou o limite de velocidade. MULTADO')
+} 
+console.log('Dirija sempre usando cinto de segurança!')
