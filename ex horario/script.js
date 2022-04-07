a41759c0d837f1c15dcf2b202cd8ef2ecd605eba
Vinicius Poleti <poleti.vinicius@gmail.com>
2022-04-07 15:39:09 -0300

    horas

---
+++ ex horario/script.js
@@ -0,0 +1,23 @@
+function carregar(){
+
+var msg = document.getElementById('msg')
+
+var img = document.getElementById('imagem')
+
+var data = new Date()
+
+var hora = data.getHours();
+
+msg.innerHTML = `Agora são ${hora} horas`
+
+if (hora >=0 && hora < 12){
+    img.scr
+
+}else if (hora >= 12 && hora < 18){
+
+}else if{
+
+}
+
+
+}
